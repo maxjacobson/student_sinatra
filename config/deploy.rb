2e341a5ec7221f3c5c4cf4e435caf3b06dcb3a30
Sarah Duve <saduve@gmail.com>
2013-06-25 15:07:29 -0400

    Fix server address in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,8 +13,8 @@ default_run_options[:pty] = true
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "199.120.16"                          # Your HTTP server, Apache/etc
-role :app, "199.120.16"                          # This may be the same as your `Web` server
+role :web, "198.199.120.16"                          # Your HTTP server, Apache/etc
+role :app, "198.199.120.16"                          # This may be the same as your `Web` server
 # role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
 # role :db,  "your slave db-server here"
 
