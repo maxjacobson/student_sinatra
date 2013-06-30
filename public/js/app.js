e9b198f7755efe3ffada1d432775ee1b85da8d8f
Max Jacobson <maxwell.jacobson@gmail.com>
2013-06-29 20:23:00 -0400

    got a search box to filter stuff out

---
+++ public/js/app.js
@@ -0,0 +1,33 @@
+// the dollar sign wasn't being recognized so I'm passing it in
+// some conflict maybe
+jQuery(function($) {
+
+  var student_query, student_name, student_query_pattern;
+
+  $("#find_student_query").on("keyup", function(){
+
+    student_query = $(this).val().toLowerCase();
+    student_query_pattern = new RegExp(student_query);
+
+    $(".big-comment").each(function() {
+
+      student_name = $(this).text().toLowerCase();
+      $student_box = $(this).closest(".home-blog-post");
+
+      if(student_query_pattern.test(student_name)) {
+
+        $student_box.fadeIn();
+
+      } else {
+
+        $student_box.fadeOut();
+
+      }
+
+    }); // end each
+
+  }); // end keyup
+
+
+
+});
